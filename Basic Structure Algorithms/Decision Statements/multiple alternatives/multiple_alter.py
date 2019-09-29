eeb9951ba4dab75652b41a9882de3a18427babbb
Hassan Farid <hassan.far.661@gmail.com>
2019-09-30 01:23:16 +0500

    python pseudo 0.4.3

---
+++ Basic Structure Algorithms/Decision Statements/multiple alternatives/multiple_alter.py
@@ -0,0 +1,16 @@
+#Algo 0.4.3: Using Multiple Alternatives
+
+#Step 01: Reading input
+VAR = int(input("Enter number: "))
+#Step 02: Using Multiple Alternatives
+#[a. Checking if greater than 0]
+if VAR > 0:
+    print(VAR, " is greater than 0")
+#[b. Checking if less than 0]
+elif VAR < 0:
+    print(VAR, " is less than 0")
+#[c. Declaring as 0]
+else:
+    print(VAR, " is equal to 0")
+
+    
