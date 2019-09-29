296096372b404408068cfdde2c4a600601902cd2
Hassan Farid <hassan.far.661@gmail.com>
2019-09-30 01:29:44 +0500

    python pseudo 0.5.2

---
+++ Basic Structure Algorithms/Iteration Statements/while loop/while_loop.py
@@ -0,0 +1,11 @@
+#Algo 0.5.2: Using while loop iteration
+
+#Step 01: Reading input
+VAR = int(input("Enter number: "))
+#Step 02: Assigning Condition Variable
+K = 1
+#Step 03: Using WHILE loop
+while K <= 10:
+    print(VAR, " * ", K, " = ", VAR*K)
+
+    
