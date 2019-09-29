94ec5b3e000d8ef8d3f9b4236d01d37897fc5129
Hassan Farid <hassan.far.661@gmail.com>
2019-09-30 01:08:34 +0500

    python pseudo 0.4.1

---
+++ Basic Structure Algorithms/Decision Statements/single alternative/single_alter.py
@@ -0,0 +1,9 @@
+#Algo 0.4.1: Using Single Alternative
+
+#Step 01: Reading input
+VAR = int(input("Enter variable to be checked: "))
+#Step 02: Checking if even
+if VAR % 2 == 0:
+    print(VAR, "is an Even number")
+
+
