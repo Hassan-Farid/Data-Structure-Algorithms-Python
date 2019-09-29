e0dbb9dd290d24fed8765ba3810d5802463b0ab4
Hassan Farid <hassan.far.661@gmail.com>
2019-09-30 01:12:44 +0500

    python pseudo 0.4.2

---
+++ Basic Structure Algorithms/Decision Statements/double alternative/double_alter.py
@@ -0,0 +1,12 @@
+#ALgo 0.4.2: Using Double Alternative
+
+#Step 01: Reading input
+VAR = int(input("Enter number to be checked: "))
+#Step 02: Applying Double Alternative
+#[a. Checking if Even]
+if VAR % 2 == 0:
+    print(VAR, " is an Even number.")
+#[b. Declaring an Odd]
+else:
+    print(VAR, " is an Odd number.")
+
