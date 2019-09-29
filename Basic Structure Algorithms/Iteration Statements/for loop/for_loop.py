248134a2b942e94c0c64a203ea11f814dc9966eb
Hassan Farid <hassan.far.661@gmail.com>
2019-09-30 01:26:25 +0500

    python pseudo 0.5.1

---
+++ Basic Structure Algorithms/Iteration Statements/for loop/for_loop.py
@@ -0,0 +1,8 @@
+#Algo 0.5.1: Using for loop iteration
+
+#Step 01: Reading input
+VAR = int(input("Enter number: "))
+#Step 02: Using for loop
+for k in range(1, 11, 1):
+    print(VAR, " * ", k, " = ", VAR*k)
+
