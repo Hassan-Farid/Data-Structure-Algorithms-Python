aadd9488704983df02fc5b57854c11437a600e8a
Hassan Farid <hassan.far.661@gmail.com>
2019-09-30 01:04:37 +0500

    python pseudo 0.3

---
+++ Basic Structure Algorithms/Sequence Statements/operation with assignment/product.py
@@ -0,0 +1,11 @@
+#Algo 0.3: Product of two Variables
+
+#Step 01: Reading first variable
+VAR1 = int(input())
+#Step 02: Assigning value to second variable
+VAR2 = 2
+#Step 03: Assigning product of variables
+VAR = VAR1 * VAR2
+#Step 04: Displaying product
+print(VAR)
+
